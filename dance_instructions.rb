5506eb6a5701c6634ba04b342125e9d64f9f61bf
Victoria Johnston <victoria13johnston@gmail.com>
2018-08-07 13:37:46 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,4 +1,3 @@
-
 # Code your methods below
 def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
@@ -8,5 +7,5 @@ def base_footwork
   puts "Lift right foot"
   puts "Return right foot to the ground"
   #finish step 3
-  skip_step("left foot")
+
 end
